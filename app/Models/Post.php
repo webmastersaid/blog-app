bf9c9459ca2fa1ea31f46102785ffbe9d341faf8
Said <46088969+webmastersaid@users.noreply.github.com>
2023-10-22 13:55:06 +0500

    add methods likedUsers, comment, parseDate

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -21,6 +24,23 @@ class Post extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function likedUsers() : BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'post_user_likes');
+    }
+
+    public function comments() : HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function parseDate()
+    {
+        $date = Carbon::parse($this->created_at);
+        $date = $date->translatedFormat('F') . ' ' . $date->day . ', ' . $date->year . ' ' . $date->format('H:i');
+        return $date;
     }
 
 }
