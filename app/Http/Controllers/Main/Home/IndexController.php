118002541a82f56a35bd2134983f1ba853bf2233
Said <46088969+webmastersaid@users.noreply.github.com>
2023-10-22 13:58:00 +0500

    show posts

--- app/Http/Controllers/Main/Home/IndexController.php
+++ app/Http/Controllers/Main/Home/IndexController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Main\Home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Carbon\Carbon;
 
 class IndexController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Post $posts)
     {
-        return view('main.home.index');
+        $sortedPosts = $posts->get()->sortBy('created_at');
+        $headerPosts = $sortedPosts->take(1);
+        $randomPosts = $posts->where('id', '!=', $headerPosts[0]->id)->get()->random(2);
+        return view('main.home.index', compact('sortedPosts', 'headerPosts', 'randomPosts'));
     }
 }
