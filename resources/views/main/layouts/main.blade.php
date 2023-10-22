770a75fff2c038c113db99bd2099ed5c50d2b344
Said <46088969+webmastersaid@users.noreply.github.com>
2023-10-22 13:53:36 +0500

    remove blog route

--- resources/views/main/layouts/main.blade.php
+++ resources/views/main/layouts/main.blade.php
@@ -141,7 +141,6 @@
         <div class="nav-scroller py-1 mb-3 border-bottom">
             <nav class="nav nav-underline justify-content-between">
                 <a class="nav-item nav-link link-body-emphasis active" href="{{ route('main.home.index') }}">Home</a>
-                <a class="nav-item nav-link link-body-emphasis" href="{{ route('main.blog.index') }}">Blog</a>
                 <a class="nav-item nav-link link-body-emphasis" href="#">Technology</a>
                 <a class="nav-item nav-link link-body-emphasis" href="#">Design</a>
                 <a class="nav-item nav-link link-body-emphasis" href="#">Culture</a>
